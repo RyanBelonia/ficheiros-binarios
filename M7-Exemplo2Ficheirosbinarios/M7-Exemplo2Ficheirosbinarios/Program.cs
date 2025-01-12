1596214a947993d597c8c58149dd4e82fef93fce
RyanBelonia <153557151+RyanBelonia@users.noreply.github.com>
2025-01-12 22:30:32 +0000

    Add files via upload

---
+++ M7-Exemplo2Ficheirosbinarios/M7-Exemplo2Ficheirosbinarios/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace M7_Exemplo2Ficheirosbinarios
+{
+    internal class Program
+    {
+        const string caminho = "nomedoficheiro.dat";
+        static void Main(string[] args)
+        {
+            EscreverFicheiro();
+            LerFicheiro();
+            Console.ReadKey();
+        }
+        public static void EscreverFicheiro()
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Open (caminho,FileMode.Create)))
+            {
+                bw.Write(1.259F); // preço do produto
+                bw.Write("caderno");
+                bw.Write(10);
+                bw.Write(true);
+            }
+            
+        }
+        public static void LerFicheiro()
+        {
+            float preco;
+            string designacao;
+            int qtd;
+            bool comArgolas;
+            if (File.Exists(caminho))
+            {
+                using (BinaryReader br = new BinaryReader(File.Open(caminho, FileMode.Open)))
+                {
+                    preco = br.ReadSingle();
+                    designacao = br.ReadString();
+                    qtd = br.ReadInt32();
+                    comArgolas = br.ReadBoolean();
+                }
+                Console.WriteLine("Preço do produto: " + preco);
+                Console.WriteLine("Designação do produto: " + designacao);
+                Console.WriteLine("Quantidade em stock: " + qtd);
+                Console.WriteLine("Com argolas na capa: " + comArgolas);
+            }
+        }
+
+    }
+}
